16a90231483bf5fbcdc36ede59c72660d9483223
Logan Nelson <Lnelson0915@gmail.com>
2025-07-07 14:50:41 -0700

    Refactor Die class to use MAX_SIDES constant
    Updated the `Die` class in `Die.cs` to introduce a constant `MAX_SIDES` set to 20, enhancing maintainability. The
     constructor's validation logic now references 'MAX_SIDES' instead of a hardcoded value, allowing for easier future adjustments to the maximum sides.

--- DiceRoller/Die.cs
+++ DiceRoller/Die.cs
@@ -17,6 +17,7 @@ namespace DiceRoller;
 
 public class Die
 {
+    private const byte MAX_SIDES = 20;
     private static readonly Random rand = new();
 
     /// <summary>
@@ -29,7 +30,7 @@ public class Die
     /// <exception cref="ArgumentOutOfRangeException">Thrown when invalid number of sides is provided</exception>
     public Die(byte numSides)
     {
-        if (numSides <= 0 || numSides > 20)
+        if (numSides <= 0 || numSides > MAX_SIDES)
         {
             throw new ArgumentOutOfRangeException(nameof(numSides)
                 , $"{nameof(numSides)} must be greater than 0 and less than 21");
